f934468490291b26e78b5cf87c4b3d440372a727
Alex Pshul <alex@pshul.com>
2022-10-26 00:53:06 -0400

    Removed unused usings

--- CustomBindingAzureFunctions/GeekJokeBinding/GeekJokeInputConverter.cs
+++ CustomBindingAzureFunctions/GeekJokeBinding/GeekJokeInputConverter.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host.Bindings;
 using Microsoft.Extensions.Logging;
 
 namespace CustomBindingAzureFunctions.GeekJokeBinding;
